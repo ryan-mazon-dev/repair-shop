b079febc4c333cb1d5130e2d7c376bc03de947db
Ryan Mazon <ryanmazonaws@protonmail.com>
2024-12-11 08:22:17 -0500

    add sentry to err

--- src/app/(rs)/error.tsx
+++ src/app/(rs)/error.tsx
@@ -1,5 +1,6 @@
 'use client' // Error boundaries must be Client Components
 
+import * as Sentry from '@sentry/nextjs'
 import { useEffect } from 'react'
 
 export default function Error({
@@ -12,6 +13,7 @@ export default function Error({
   useEffect(() => {
     // Log the error to an error reporting service
     console.error(error)
+    Sentry.captureException(error)
   }, [error])
 
   return (
